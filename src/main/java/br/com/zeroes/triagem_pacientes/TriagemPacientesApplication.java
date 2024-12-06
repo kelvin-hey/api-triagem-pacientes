6213fe4975a8e80e40abd318767c9c448ad79598
Kelvin Hey <kelvinhey@outlook.com>
2024-12-05 21:25:26 -0300

    feat: adiciona arquivos iniciais do projeto

---
+++ src/main/java/br/com/zeroes/triagem_pacientes/TriagemPacientesApplication.java
@@ -0,0 +1,13 @@
+package br.com.zeroes.triagem_pacientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriagemPacientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TriagemPacientesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zeroes/triagem_pacientes/TriagemPacientesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zeroes.triagem_pacientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriagemPacientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
