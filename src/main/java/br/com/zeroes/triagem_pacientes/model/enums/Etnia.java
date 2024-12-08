77d1fef11d52beb7278c6ea43b745c8c263c5606
Kelvin Hey <kelvinhey@outlook.com>
2024-12-08 11:57:23 -0300

    feat: adiciona tipos ENUM para a classe Paciente

---
+++ src/main/java/br/com/zeroes/triagem_pacientes/model/enums/Etnia.java
@@ -0,0 +1,9 @@
+package br.com.zeroes.triagem_pacientes.model.enums;
+
+public enum Etnia {
+    BRANCA,
+    PRETA,
+    PARDA,
+    AMARELA,
+    INDIGENA
+}
---
+++ src/main/java/br/com/zeroes/triagem_pacientes/model/enums/Situacao.java
@@ -0,0 +1,29 @@
+package br.com.zeroes.triagem_pacientes.model.enums;
+
+public enum Situacao {
+    AGUARDANDO("Aguardando Recepção"),
+    SENDO_RECEPCIONADO("Sendo Recepcionado"),
+    AGUARDANDO_ACOLHIMENTO("Aguardando Acolhimento"),
+    SENDO_ACOLHIDO("Sendo Acolhido"),
+    AGUARDANDO_CLASSIFICACAO_RISCO("Aguardando Classificação de Risco"),
+    PASSANDO_CLASSIFICACAO("Passando por Classificação"),
+    AGUARDANDO_ATENDIMENTO_MEDICO("Aguardando Atendimento Médico"),
+    EM_ATENDIMENTO_MEDICO("Em Atendimento Médico"),
+    RETORNO_CONSULTA("Retorno Consulta"),
+    AGUARDANDO_ATENDIMENTO_ENFERMAGEM("Aguardando Atendimento de Enfermagem"),
+    EM_ATENDIMENTO_ENFERMAGEM("Em Atendimento de Enfermagem"),
+    ENCAMINHAMENTO("Encaminhamento"),
+    DISPENSADO("Dispensado"),
+    ATENDIMENTO_CONCLUIDO("Atendimento concluído"),
+    ATRIBUIDO_LEITO("Atribuido leito");
+
+    private String descricao;
+
+    Situacao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+}
